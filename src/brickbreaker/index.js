60b44193827e65fc47e396eec4889076981fc516
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-13 14:12:48 -0400

    32:20 on the youtube video im coding along with

---
+++ src/brickbreaker/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./Board";
+const BrickBreaker = () => {
+
+    return (
+        <div>
+            <Board></Board>
+        </div>
+    );
+
+}
+
+
+export default BrickBreaker
